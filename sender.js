73e2472930b97d32580f19b213654deb51dfdff0
smllmyh <samuel.alemayehu777@gmail.com>
2024-02-11 12:08:24 +0300

    Added telegram bot to send messege if she says yes

---
+++ sender.js
@@ -0,0 +1,33 @@
+//bot token
+var telegram_bot_id = "6571511177:AAH8-6ZY7UkeI9jjtw23CFA3H5EYz3LaM4E";
+//chat id
+var chat_id = -4134023149;
+var message;
+var ready = function () {
+  message = document.getElementById("message").innerHTML =
+    "She said yes from valentine";
+  message = "Message: " + message;
+};
+var sender = function () {
+  ready();
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache",
+    },
+    data: JSON.stringify({
+      chat_id: chat_id,
+      text: message,
+    }),
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+  document.getElementById("message").value = "";
+  return false;
+};
